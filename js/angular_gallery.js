5d1b042b4f572f0f780a24f5f22331aa221abdc5
Stavros Ioannidis <StavrosIoannidis@Stavs-Mac-mini.local>
2015-10-30 18:48:50 +0200

    Initial code commit

---
+++ js/angular_gallery.js
@@ -0,0 +1,63 @@
+// The angular-drupal configuration settings for my simple app.
+// angular.module('angular-drupal').config(function($provide) {
+
+//   $provide.value('drupalSettings', {
+//     sitePath: 'http://istav.gr/meetup7',
+//     endpoint: 'api'
+//   });
+
+// });
+
+// My simple app.
+
+// angular.module('angular_gallery', ['iso.directives'])
+angular.module('angular_gallery', ['wu.masonry'])
+.controller('GalleryController', ['$scope', '$http', function($scope, $http) {
+  var path = 'api/gallery', 
+    loadMoreTimer, 
+    page = -1, 
+    pages = 1,
+    infiniteScrollDisabled = false;
+
+  $scope.images = [];
+
+  jQuery(function($){
+    $(window).scroll(function() {
+      if($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
+        clearTimeout(loadMoreTimer);
+        loadMoreTimer = setTimeout(function(){
+          loadMore();
+        },0);
+      }
+    });
+    $('#isotopeContainer').on( 'layoutComplete', function( event, laidOutItems ) {
+      $(window).trigger('resize');
+    } )
+  });
+
+  function loadMore() {
+    if (!infiniteScrollDisabled){
+      if (page == pages - 1){
+        infiniteScrollDisabled = true;
+        return;
+      }
+      infiniteScrollDisabled = true;
+      $http.get(path + '?page=' + (page + 1))
+      .success(function(data){
+        [].push.apply($scope.images, data.images);
+        page = data.view.page;
+        pages = data.view.pages;
+        infiniteScrollDisabled = false;
+        setTimeout(function(){
+          jQuery("img.lazy").lazyload({
+            threshold : 200,
+            failure_limit : data.view.limit
+          });
+          jQuery('a.angular-gallery-image').colorbox({rel:'gal', maxHeight: '90%'});
+        },0);
+      });
+    }
+  };
+
+  loadMore();
+}]);
